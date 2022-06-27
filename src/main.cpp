a54118baebe2628ce66fb8efd45b4a4c5ed5b471
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2022-06-27 21:46:31 +0200

    start project

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// Code by Thorsten Kattanek //
+// A consol tool to create a mif (memory init file) for fpga and eeprom. //
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
